9c10ce096bd2c2f1a6a91129f9872275fa4f16d9
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 15:19:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@ cd /tmp \
     && mkdir -p /etc/stubby \
     && cp -n ./stubby.yml /etc/stubby/ \
     && rm -f ./stubby.yml
+
+# run stubby as service
+mkdir -p /etc/services.d/stubby \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/run \
+    && echo 's6-echo "Starting stubby"' >> /etc/services.d/stubby/run \
+    && echo 'stubby -C /etc/stubby/stubby.yml' >> /etc/services.d/stubby/run \
+    && echo '#!/usr/bin/with-contenv bash' > /etc/services.d/stubby/finish \
+    && echo 's6-echo "Stopping stubby"' >> /etc/services.d/stubby/finish \
+    && echo 'killall -9 stubby' >> /etc/services.d/stubby/finish
