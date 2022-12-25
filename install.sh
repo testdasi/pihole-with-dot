2b50abbac100180df4cc732be8aa5e4a805aaad5
testdasi <51292604+testdasi@users.noreply.github.com>
2022-12-25 22:42:04 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,5 @@ cd /tmp \
     && rm -f ./stubby.yml
 
 ## Piggy-backing on Pihole service ##
-# Insert run lines below the call capsh comment
-sed -i "/^# Call capsh with the detected capabilities/a stubby -g -C \/etc\/stubby\/stubby.yml" /etc/s6-overlay/s6-rc.d/pihole-FTL/run
-# Insert finish lines above kill pihole
-sed -i "/^killall -15 pihole-FTL/i killall -15 stubby" /etc/s6-overlay/s6-rc.d/pihole-FTL/finish
+sed -i "/^lighttpd /i stubby -g -C \/config\/stubby.yml" /etc/s6-overlay/s6-rc.d/lighttpd/run
+sed -i "/^killall -9 lighttpd/i killall stubby" /etc/s6-overlay/s6-rc.d/lighttpd/finish
